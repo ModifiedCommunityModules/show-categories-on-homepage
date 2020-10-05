ed51d1c0e24e2e03d03397965020506af2fa368d
Gulliver72 <31593252+Gulliver72@users.noreply.github.com>
2020-10-05 23:07:36 +0200

    Update categories_list.php
    corrected authors

--- includes/extra/default/center_modules/categories_list.php
+++ includes/extra/default/center_modules/categories_list.php
@@ -1,8 +1,10 @@
 <?php
     /* MODUL: Kategorien auf der Startseite anzeigen (c_list Modul)
-     * 
-     * Anpassung an modified 1.x: sgei
-     * Anpassung an modified 2.x: |Alex|
+     * 2020 by Gulliver72 - transferred to Github and entered in MMLC
+     * 2017 weitere Anpassung an PHP7: awids
+     * 2016 Anpassung an modified 2.x und PHP7: |Alex|
+     * 2011 Aktualisierung: koshiro aka Micha und mrsmartmouse
+     * 2009 Anpassung an modified 1.x: sgei
      * Quelle: xtc-load.de - Autor: unbekannt
      */
    

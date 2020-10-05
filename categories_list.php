6a47aad125f9a1e096e2ab64e9fe510734f4c3e1
Gulliver72 <31593252+Gulliver72@users.noreply.github.com>
2020-10-05 19:42:10 +0200

    Add files via upload

---
+++ categories_list.php
@@ -0,0 +1,10 @@
+<?php
+	// MODUL: Kategorien auf der Startseite anzeigen (c_list Modul)
+	// Anpassung an modified 1.x: sgei
+	// Anpassung an modified 2.x: |Alex|
+	// Quelle: xtc-load.de - Autor: unbekannt
+	
+	require_once(DIR_WS_MODULES . FILENAME_CATEGORIES_LIST);
+	
+	$default_smarty->assign('CATEGORIES_LIST', $module_categories);
+?>
